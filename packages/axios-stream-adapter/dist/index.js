31511ad5310ed3ab9794038d167756327fbd8723
sunkeysun <sunkeysun@tencent.com>
2023-05-30 17:42:07 +0800

    feat: 修改 headers 类型

--- packages/axios-stream-adapter/dist/index.js
+++ packages/axios-stream-adapter/dist/index.js
@@ -2,8 +2,9 @@ import { __awaiter, __rest } from "tslib";
 /**
  * sse 适配器
  */
-import axios, { AxiosError } from 'axios';
+import axios, { AxiosError, AxiosHeaders, } from 'axios';
 const CONTENT_JSON = 'application/json';
+const CONTENT_TEXT = 'text/plain';
 const CONTENT_EVENT_STREAM = 'text/event-stream';
 const streamAdapter = function streamAdapter(config) {
     return __awaiter(this, void 0, void 0, function* () {
@@ -11,20 +12,23 @@ const streamAdapter = function streamAdapter(config) {
         const fullUrl = axios.getUri(config);
         const abortController = new AbortController();
         let timer;
-        if (timeout && !signal) {
+        if (timeout) {
+            if (signal) {
+                signal.onabort = () => {
+                    abortController.abort();
+                };
+            }
             timer = setTimeout(() => abortController.abort(), timeout);
         }
         try {
             const res = yield fetch(fullUrl, Object.assign(Object.assign({}, rest), { headers,
-                method, body: data, signal: (signal !== null && signal !== void 0 ? signal : abortController.signal) }));
+                method, body: data, signal: abortController.signal }));
             const statusCode = res.status;
             const response = {
                 data: res.body,
                 status: statusCode,
                 statusText: res.statusText,
-                headers: Object.fromEntries([
-                    ...res.headers,
-                ]),
+                headers: new AxiosHeaders(Object.fromEntries(res.headers.entries())),
                 config,
                 request: null,
             };
@@ -35,42 +39,48 @@ const streamAdapter = function streamAdapter(config) {
             if (!res.body) {
                 return response;
             }
-            if (res.headers.get('content-type') === CONTENT_JSON) {
-                response.data = yield res.json();
-            }
-            else if (res.headers.get('content-type') === CONTENT_EVENT_STREAM) {
-                response.data = new ReadableStream({
-                    start(controller) {
-                        return __awaiter(this, void 0, void 0, function* () {
-                            if (!res.body) {
-                                return controller.close();
-                            }
-                            const reader = res.body.getReader();
-                            while (true) {
-                                const { value, done } = yield reader.read();
-                                const decoder = new TextDecoder();
-                                const lines = decoder.decode(value).split('\n\n');
-                                lines.forEach((line) => {
-                                    const [, data] = line.split('data: ');
-                                    if (data && data !== '[DONE]') {
-                                        let parsedData = data;
-                                        try {
-                                            parsedData = JSON.parse(data);
+            const contentType = res.headers.get('content-type');
+            switch (contentType) {
+                case CONTENT_JSON:
+                    response.data = yield res.json();
+                    break;
+                case CONTENT_TEXT:
+                    response.data = yield res.text();
+                    break;
+                case CONTENT_EVENT_STREAM:
+                    response.data = new ReadableStream({
+                        start(controller) {
+                            var _a;
+                            return __awaiter(this, void 0, void 0, function* () {
+                                const reader = (_a = res.body) === null || _a === void 0 ? void 0 : _a.getReader();
+                                if (!reader)
+                                    return;
+                                while (true) {
+                                    const { value, done } = yield reader.read();
+                                    const decoder = new TextDecoder();
+                                    const lines = decoder.decode(value).split('\n\n');
+                                    lines.forEach((line) => {
+                                        const [, data] = line.split('data: ');
+                                        if (data && data !== '[DONE]') {
+                                            let parsedData = data;
+                                            try {
+                                                parsedData = JSON.parse(data);
+                                            }
+                                            catch (err) {
+                                                // empty
+                                            }
+                                            controller.enqueue(parsedData);
                                         }
-                                        catch (err) {
-                                            // empty
-                                        }
-                                        controller.enqueue(parsedData);
+                                    });
+                                    if (done) {
+                                        controller.close();
+                                        break;
                                     }
-                                });
-                                if (done) {
-                                    controller.close();
-                                    break;
                                 }
-                            }
-                        });
-                    }
-                });
+                            });
+                        },
+                    });
+                    break;
             }
             return response;
         }
--- packages/axios-stream-adapter/src/index.ts
+++ packages/axios-stream-adapter/src/index.ts
@@ -1,83 +1,105 @@
 /**
  * sse 适配器
  */
-import axios, { AxiosError, type AxiosAdapter } from 'axios';
-const CONTENT_JSON = 'application/json';
-const CONTENT_EVENT_STREAM = 'text/event-stream';
+import axios, {
+  AxiosError,
+  type AxiosAdapter,
+  AxiosResponse,
+  AxiosHeaders,
+} from 'axios'
+const CONTENT_JSON = 'application/json'
+const CONTENT_TEXT = 'text/plain'
+const CONTENT_EVENT_STREAM = 'text/event-stream'
 
 const streamAdapter: AxiosAdapter = async function streamAdapter(config) {
-    const { data, headers, method, signal, validateStatus, timeout, ...rest } = config;
-    const fullUrl = axios.getUri(config);
-    const abortController = new AbortController();
-    let timer;
-    if (timeout && !signal) {
-        timer = setTimeout(() => abortController.abort(), timeout);
+  const { data, headers, method, signal, validateStatus, timeout, ...rest } =
+    config
+  const fullUrl = axios.getUri(config)
+  const abortController = new AbortController()
+  let timer
+  if (timeout) {
+    if (signal) {
+      signal.onabort = () => {
+        abortController.abort()
+      }
     }
-    try {
-        const res = await fetch(fullUrl, {
-            ...rest,
-            headers,
-            method,
-            body: data,
-            signal: (signal ?? abortController.signal) as AbortSignal,
-        })
-        const statusCode = res.status;
-        const response = {
-            data: res.body as ReadableStream<unknown>,
-            status: statusCode,
-            statusText: res.statusText,
-            headers: Object.fromEntries([
-                ...(res.headers as unknown as Map<string, string>),
-            ]),
-            config,
-            request: null,
-        };
-        if (!res.ok || (validateStatus && !validateStatus(statusCode))) {
-            response.data = await res.json()
-            throw new AxiosError(`Request failed with status code ${statusCode}`, String(statusCode), config, null, response);
-        }
-        if (!res.body) {
-            return response
-        }
+    timer = setTimeout(() => abortController.abort(), timeout)
+  }
+  try {
+    const res = await fetch(fullUrl, {
+      ...rest,
+      headers,
+      method,
+      body: data,
+      signal: abortController.signal,
+    })
+    const statusCode = res.status
+    const response: AxiosResponse = {
+      data: res.body as ReadableStream<unknown>,
+      status: statusCode,
+      statusText: res.statusText,
+      headers: new AxiosHeaders(Object.fromEntries(res.headers.entries())),
+      config,
+      request: null,
+    }
+    if (!res.ok || (validateStatus && !validateStatus(statusCode))) {
+      response.data = await res.json()
+      throw new AxiosError(
+        `Request failed with status code ${statusCode}`,
+        String(statusCode),
+        config,
+        null,
+        response,
+      )
+    }
+    if (!res.body) {
+      return response
+    }
 
-        if (res.headers.get('content-type') === CONTENT_JSON) {
-            response.data = await res.json();
-        } else if (res.headers.get('content-type') === CONTENT_EVENT_STREAM) {
-            response.data = new ReadableStream<string | Record<string, unknown>>({
-                async start(controller) {
-                    if (!res.body) {
-                        return controller.close()
-                    }
-                    const reader = res.body.getReader()
-                    while (true) {
-                        const { value, done } = await reader.read()
-                        const decoder = new TextDecoder()
-                        const lines = decoder.decode(value).split('\n\n')
-                        lines.forEach((line) => {
-                            const [, data] = line.split('data: ')
-                            if (data && data !== '[DONE]') {
-                                let parsedData = data
-                                try {
-                                    parsedData = JSON.parse(data)
-                                } catch (err) {
-                                    // empty
-                                }
-                                controller.enqueue(parsedData)
-                            }
-                        })
-                        if (done) {
-                            controller.close()
-                            break
-                        }
-                    }
+    const contentType = res.headers.get('content-type')
+
+    switch (contentType) {
+      case CONTENT_JSON:
+        response.data = await res.json()
+        break
+      case CONTENT_TEXT:
+        response.data = await res.text()
+        break
+      case CONTENT_EVENT_STREAM:
+        response.data = new ReadableStream<string | Record<string, unknown>>({
+          async start(controller) {
+            const reader = res.body?.getReader()
+            if (!reader) return
+            while (true) {
+              const { value, done } = await reader.read()
+              const decoder = new TextDecoder()
+              const lines = decoder.decode(value).split('\n\n')
+              lines.forEach((line) => {
+                const [, data] = line.split('data: ')
+                if (data && data !== '[DONE]') {
+                  let parsedData = data
+                  try {
+                    parsedData = JSON.parse(data)
+                  } catch (err) {
+                    // empty
+                  }
+                  controller.enqueue(parsedData)
                 }
-            })
-        }
-        return response
-    } catch (error) {
-        throw new AxiosError((error as Error).message, '0', config, null)
-    } finally {
-        clearTimeout(timer);
+              })
+              if (done) {
+                controller.close()
+                break
+              }
+            }
+          },
+        })
+        break
     }
-};
-export default streamAdapter;
+    return response
+  } catch (error) {
+    throw new AxiosError((error as Error).message, '0', config, null)
+  } finally {
+    clearTimeout(timer)
+  }
+}
+export default streamAdapter
